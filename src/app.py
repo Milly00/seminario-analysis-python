b1f54c7b335508205c966caafb4ee9f915c5cbbe
Milly <benitezmilly3@gmail.com>
2023-04-11 12:32:00 -0500

    Archivos de la integración

---
+++ src/app.py
@@ -0,0 +1,152 @@
+from flask import Flask, request,jsonify, Response
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+
+from flask_cors import CORS
+from scrap import Scrap #recordar instalar el paquete
+import datetime
+
+import pandas as pd
+
+
+app = Flask(__name__) #Instancia de una app
+#app.config['MONGO_URI']='mongodb://localhost:27017/seminario' #cadena de conexion
+app.config['MONGO_URI']='mongodb+srv://mean_user:DSZTXYsUeVc045Ox@cluster0.0bbgsoe.mongodb.net/seminario' #cadena de conexion
+CORS(app)
+
+mongo = PyMongo(app) #Pasamos la cadena de conexion para conectarnos a la BD
+
+scrap =  Scrap()
+
+@app.route('/')
+def index():
+    return '<h1>Hello!</h1>'
+
+@app.route('/analysis', methods=['POST'])
+def createPublication():
+
+    try:
+        print(request.json)
+        profile_name = request.json['profile_name']
+        name_post = request.json['name_post']
+        #name_file = request.json['name_file']
+        id_admin = request.json['id_admin']
+        descripcion = request.json['descripcion']
+        n_redsocial = request.json['n_redsocial']
+        url_publicacion = request.json['url_publicacion']
+        category = request.json['categoria']
+        data = []
+        if(n_redsocial == 'instagram'):
+            data = scrap.loadData(url_publicacion)
+
+        if(n_redsocial == 'twitter'):
+            data = scrap.loadData(url_publicacion)
+        
+        if profile_name and name_post  and id_admin and descripcion and n_redsocial and url_publicacion and category:
+            #comentar el método en caso de que no haya conx con la BD
+            
+            savePublication(data, name_post,id_admin,descripcion,n_redsocial,url_publicacion,profile_name, category)
+            #saveStatistic(data, name_post)
+            response = jsonify({
+            'message': 'Se ha registrado su publicación',
+            'status': 200,
+            'data': data
+            })
+
+            #Llamar el método de crear estadisticas
+            return response
+        else:
+            return not_found()
+    except Exception as err:
+        print(err)
+        return  service_error()
+
+@app.route('/new-statistic', methods=['POST'])
+def createStatistic():
+    try:
+        #print(request.json)
+        id_publication = request.json['name_post']
+        data = request.json['data']
+
+        new_data = scrap.generateStatistic(data)
+        #print('newdata:'+new_data)
+        saveStatistic(id_publication, new_data)
+        
+        res = {'ok': True, 'data': new_data}
+        return res
+        
+    except Exception as err:
+        print('Error createEst')
+        print(err)
+        return service_error()
+
+
+def savePublication(data,id,id_admin,descripcion,n_redsocial,url_publicacion, autor,categoria):
+        try:
+            #Inicio con la BD
+            #print(data)
+            date = datetime.datetime.now()
+            #data = scrap.analysisSentiment(url_publicacion)
+            #print(data)
+            mongo.db.publications.insert_one({"datos": data , "fpublicacion": date,"pid": id,"uid": id_admin,"numcomentarios": scrap.totalData(data), "descripcion": descripcion,"nredsocial": n_redsocial, "urlpublicacion": url_publicacion, "autor": autor,"categoria": categoria})
+        except Exception as err:
+            print('ERRROR saveP')
+            print(err)
+            return service_error()
+
+
+
+def saveStatistic(id_publication, data):
+    try:
+        #print(id_publication, data)
+        date = datetime.datetime.now()
+        data_matriz = []
+        for value in data:
+            element = [value['autor'], value['puntuacion'], value['etiqueta'], value['emocion'], value['likes'], value['fecha']]
+            data_matriz.append(element)
+        
+        df_matriz = pd.DataFrame(data_matriz, columns=["Author", "Score", "Polarity", "Emotion", "Likes", "Date"])
+        
+        description = df_matriz.describe()
+        polarity = df_matriz.Polarity.value_counts()
+        count_emotions = df_matriz.Emotion.value_counts()
+        
+        description_db = description.to_json()
+        polarity_db = polarity.to_json()
+        count_emotions_db = count_emotions.to_json()
+
+        
+        mongo.db.statistics.insert_one({"id_publication": id_publication , "date_statistic": date, "data": data, "description": description_db, "polarity": polarity_db, "count_emotions": count_emotions_db})
+        
+    except Exception as err:
+        print('ERROR saveStatistic')
+        print(err)
+        return service_error()
+
+
+
+@app.errorhandler(404)
+def not_found(error = None):
+    response = jsonify({
+        'message': 'Resource Not Found:' + request.url,
+        'status': 404
+    })
+
+    response.status_code = 404
+    return response
+
+@app.errorhandler(500)
+def service_error(error = None):
+    response = jsonify({
+        'message': 'Error:' + request.url,
+        'status': 500
+    })
+
+    response.status_code = 500
+    return response
+
+#Si esta ejecutando el archivo como modulo principal ejecutar la app
+if __name__ == "__main__":
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=False,port=8000)
---
+++ src/scrap.py
@@ -0,0 +1,207 @@
+from apify_client import ApifyClient
+import pandas as pd
+from deep_translator import GoogleTranslator
+from pysentimiento import create_analyzer
+
+import json
+
+import nltk
+import re
+#nltk.download('vader_lexicon') 
+#from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import datetime
+
+class Scrap():
+
+    def __init__(self) -> None:
+        self.api_key = "apify_api_ckB1aNrtejcdli9Qw2CHyaZ7cmop7A1n0Y4a"
+        self.analyzer = create_analyzer(task="sentiment", lang="es")
+        self.sentiment = create_analyzer(task="emotion",lang = "es")
+    
+    #Agregar variable de retorno
+    def loadData(self,url_publicacion):
+        # Initialize the ApifyClient with your API token
+        posts = []
+
+        try:
+            client = ApifyClient(self.api_key)
+        
+            #Creamos un array vacio que contendra los datos brutos
+            #serie = []
+            # Prepare the actor input
+            run_input = {
+                "directUrls": [
+                    url_publicacion,
+                ],
+                "resultsLimit": 100,
+            }
+
+            # Run the actor and wait for it to finish
+            run = client.actor("apify/instagram-comment-scraper").call(run_input=run_input)
+
+            # Fetch and print actor results from the run's dataset (if there are any)
+            try:
+                for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+                    #print(item)
+                    #Obtenemos el texto del comentario
+                    text_comment = item['text']
+                    #Traducimos el texto para un mejor analisis
+                    #translante_text = self.translate(text_comment)
+                    #analisamos el texto traducido
+                    #analysis = self.analysisSentiment(text_comment)
+                    analysis_res = self.analyzer.predict(text_comment)
+                    emotion = self.sentiment.predict(text_comment)
+                    #print(analysis_res)
+                    score = self.calculateScore(analysis_res.probas,analysis_res.output)
+
+                    temporal = {"id_comentario": item['id'],"autor": item["ownerUsername"],"likes": item["likesCount"],"original_texto": self.remove_emoji(text_comment),"puntuacion": score,"fecha": str(item["timestamp"]),"etiqueta": analysis_res.output , "res_analisis": analysis_res.probas , "emocion": emotion.output , "res_emocion": emotion.probas}
+                    posts.append(temporal)
+                    #serie.append([item['id'],item["ownerUsername"],item["likesCount"],analysis['compound'],str(item["timestamp"])])
+                
+                #df = pd.DataFrame(serie)
+                #df.to_csv('myfile.csv')
+
+                #print(posts)
+                return posts
+            except ValueError as ve:
+                print("Los datos a evaluar no tienen un formato válido")
+        except ValueError as ve:
+                print("Ha ocurrido un error al realizar el analisis, intente de nuevo")
+        
+
+    #Agregar variable de retorno
+    def loadDataTwitter(self,profile_name,title,url_publicacion):
+        # Initialize the ApifyClient with your API token
+        posts = []
+        try:
+            client = ApifyClient(self.api_key)
+            #Creamos un array vacio que contendra los datos brutos
+            #serie = []
+            # Prepare the actor input
+            run_input = {
+            "startUrls": [{ "url": "https://twitter.com/DiegoASantos/status/1638008508456091651" }],
+            "tweetsDesired": 100,
+            }
+
+            # Run the actor and wait for it to finish
+            # Run the actor and wait for it to finish
+            run = client.actor("quacker/twitter-scraper").call(run_input=run_input)
+            try:
+                # Fetch and print actor results from the run's dataset (if there are any)
+                for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+                    print(item)
+                    #Obtenemos el texto del comentario
+                    text_comment = item['full_text']
+                    #Traducimos el texto para un mejor analisis
+                    #translante_text = self.translate(text_comment)
+                    #analisamos el texto traducido
+                    #analysis = self.analysisSentiment(translante_text)
+                    analysis_res = self.analyzer.predict(text_comment)
+                    score = self.calculateScore(analysis_res.probas)
+                    emotion = self.sentiment.predict(text_comment)
+                    user = item["user"]
+                    temporal = {"id_comentario": item['conversation_id'],"autor": user["screen_name"],"likes": item["favorite_count"],"original_texto": self.remove_emoji(text_comment),"puntuacion": score,"fecha": str(item["created_at"]),"etiqueta": analysis_res.output , "res_analisis": analysis_res.probas , "emocion": emotion.output , "res_emocion": emotion.probas}
+                    posts.append(temporal)
+                    #serie.append([item['conversation_id'],user["screen_name"],item["favorite_count"],analysis['compound'],str(item["created_at"])])
+                
+                #df = pd.DataFrame(serie)
+                #df.to_csv('myfiletwitter.csv')
+
+                print(posts)
+            except:
+                print("Los datos a evaluar no tienen un formato válido")
+        except:
+            print("Ha ocurrido un error al realizar el analisis, intente de nuevo")
+        
+
+    def calculateScore(self,data,label):
+        lista = list(data.values())
+        max = 0
+        #print(lista)
+        try:
+            for i in lista:
+                #print(i)
+                if(max < i):
+                    max = i
+            
+            if(label == 'NEG'):
+                max = max * -1
+        except:
+            max = 0
+        return max
+
+    def analysisSentiment(self,data):
+        analysis = {}
+        try:
+            #Inicializamos la instancia
+            sid = SentimentIntensityAnalyzer()
+            #Calculamos la polaridad
+            analysis = sid.polarity_scores(data)
+            analysis = analysis
+        except: 
+            print("Ha ocurrido un error al analizar el tetxo")
+        finally:
+            return analysis
+  
+    def generateStatistic(self,data):
+        #puntuacion,etiqueta,emocion,anger,disgust,fear
+        #[puntuacion,etiqueta,emocion,anger]
+        #lo otro buscarlo dentro del objeto res_emocion
+        transform_data = []
+        #datapy = json.loads(data)
+        print('generate',data)
+        
+        for item in data:
+            #anger, disgust  = data.res_emocion
+            transform_data.append({"autor": item['autor'], "fecha": item['fecha'], "puntuacion": item['puntuacion'], "etiqueta": item['etiqueta'], "emocion": item['emocion'], "likes": item['likes'], "anger": item['res_emocion']['anger'], "disgust": item['res_emocion']['disgust'], "fear": item['res_emocion']['fear'], "joy": item['res_emocion']['joy'], "others": item['res_emocion']['others'], "sadness": item['res_emocion']['sadness'], "surprise": item['res_emocion']['surprise']})
+        print('transform',transform_data)
+        return transform_data
+
+    def translate(self,data):
+        try:
+            traductor = GoogleTranslator(source='es', target='en')
+            result = traductor.translate(data)
+            #Limpiamos el texto, eliminado los emojis
+            r_result = self.remove_emoji(str(result)) 
+            return r_result
+        except:
+            print("No se pudo realizar la traducción")
+    
+    def remove_emoji(self,string):
+
+        try:
+            emoji_pattern = re.compile("["
+                                        u"\U0001F600-\U0001F64F"  # emoticons
+                                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                        u"\U00002500-\U00002BEF"  # chinese char
+                                        u"\U00002702-\U000027B0"
+                                        u"\U00002702-\U000027B0"
+                                        u"\U000024C2-\U0001F251"
+                                        u"\U0001f926-\U0001f937"
+                                        u"\U00010000-\U0010ffff"
+                                        u"\u2640-\u2642"
+                                        u"\u2600-\u2B55"
+                                        u"\u200d"
+                                        u"\u23cf"
+                                        u"\u23e9"
+                                        u"\u231a"
+                                        u"\ufe0f"  # dingbats
+                                        u"\u3030"
+                                        u"\u002c"
+                                        u"\u002e"
+                                        u"\u0022"
+                                        u"\u0040"
+                                        "]+", flags=re.UNICODE)
+            return emoji_pattern.sub(r'', string)
+        except:
+            print("No se pudo remover el caracter")
+
+    def totalData(seld, data):
+        total = len(data)
+        return total
+if __name__=="__main__":
+    cls = Scrap()
+    #title = "https://www.instagram.com/p/"
+    #cls.loadData('unicor_sexy',title,'https://www.instagram.com/p/CpwLdMOA5us/')
